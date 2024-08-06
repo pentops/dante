82be9df159b311aba24b1bbdcf53580ee535cc01
Damien Whitten <dw@chaos404.com>
2024-08-06 09:47:59 -0700

    Update Deps

--- service/grpc.go
+++ service/grpc.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"github.com/pentops/go-grpc-helpers/protovalidatemw"
 	"github.com/pentops/log.go/grpc_log"
 	"github.com/pentops/log.go/log"
 	"github.com/pentops/o5-auth/o5auth"
@@ -11,7 +10,6 @@ import (
 func GRPCMiddleware() []grpc.UnaryServerInterceptor {
 	return []grpc.UnaryServerInterceptor{
 		grpc_log.UnaryServerInterceptor(log.DefaultContext, log.DefaultTrace, log.DefaultLogger),
-		protovalidatemw.UnaryServerInterceptor(),
 		o5auth.GRPCMiddleware,
 	}
 }
